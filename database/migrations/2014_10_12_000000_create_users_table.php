cad9b5351a525ea26403c024d5bfec65bf14450f
moinulibr <moinulibr@gmail.com>
2022-04-15 16:00:53 +0600

    some changes to user migration file

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,8 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name')->nullable();
+            $table->string('email')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->tinyInteger('verified')->default(0);
             $table->tinyInteger('status')->default(0);
